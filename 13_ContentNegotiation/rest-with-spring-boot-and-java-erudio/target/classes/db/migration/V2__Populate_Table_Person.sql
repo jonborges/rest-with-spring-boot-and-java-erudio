fdf6dbbfbb61eb61dd4c954d5bc4c0021b603495
JonathanB <86327641+jonborges@users.noreply.github.com>
2025-08-31 01:58:20 -0300

    Aula 13

---
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/target/classes/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Rua B', 'Jonathan', 'Homem', '12'),
+	(2, 'Em cima da mesa', 'Tonhao', 'Gato', 'Lindinho'),
+	(4, 'sei la', 'Joaozinho', 'male', 'Oiii'),
+	(5, 'sei la', 'Joaozinho', 'male', 'Oiii'),
+	(6, 'sei la', 'Joaozinhor', 'male', 'feito 2'),
+	(7, 'sei la', 'Joaozinhor', 'male', 'feito 2');
